53569a8511667f69da4382b97e8b6e58f9c04170
derradjakli <derradjakli@hotmail.com>
2019-01-22 16:51:57 +0100

    City Edited

---
+++ src/main/java/fr/dauphine/ja/student/pandemiage/gameengine/City.java
@@ -0,0 +1,94 @@
+package fr.dauphine.ja.student.pandemiage.gameengine;
+
+import java.util.List;
+
+public class City {
+	private String CityName;
+	private int r;// The Red color of RGB
+	private int g;// The GREEN color of RGB
+	private int b;// The BLUE color of RGB
+	private double weight;
+	private float x;
+	private float y;
+	private float size;
+	private double eigenvector_Centrality;
+	private String edge_ID;
+	private String edge_Label;
+	private int degree; // Neighbours number
+	
+	private List<City> list;
+	
+	public City(String CityName, int r	,int g, int b, double weight, float x, float y, float size,double eigenvector_Centrality	,String edge_ID,String edge_Label	, int degree , List<City> list) {
+		this.CityName=CityName;
+		this.r=r;// The Red color of RGB
+		this.g=g;// The GREEN color of RGB
+		this.b=b;// The BLUE color of RGB
+		this.weight=weight;
+		this.x=x;
+		this.y=y;
+		this.size=size;
+		this.eigenvector_Centrality=eigenvector_Centrality;
+		this.edge_ID=edge_ID;
+		this.edge_Label=edge_Label;
+		this.degree=degree; // Neighbours number
+		
+		this.list=list;
+		
+	}
+	
+	public String getName() {
+		return CityName;
+	}
+	public int getR() {
+		return r;
+	}
+	public int getG() {
+		return g;
+	}
+	public int getB() {
+		return b;
+	}
+	public float getSize() {
+		return size;
+	}
+	public double getEin_Cent() {
+		return this.eigenvector_Centrality;
+	}
+	public String getEdg_ID() {
+		return this.edge_ID;
+	}
+	public String getEdge_Label() {
+		return this.edge_Label;
+	}
+	public int getDegree() {
+		return this.degree;
+	}
+	public void setName(String name) {
+		this.CityName=name;
+	}
+	public void setR(int r) {
+		this.r=r;
+	}
+	public void setG(int g) {
+		this.g=g;
+	}
+	public void setB(int b) {
+		this.b=b;
+	}
+	public void setSize(float s) {
+		this.size=s;
+	}
+	public void setEin_Cent(double d) {
+		this.eigenvector_Centrality=d;
+	}
+	public void setEdg_ID(String s) {
+		this.edge_ID=s;
+	}
+	public void setEdge_Label(String s) {
+		this.edge_Label=s;
+	}
+	public void setDegree(int d) {
+		this.degree=d;
+	}
+	
+}
